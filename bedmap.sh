be1a67427b5ec8d386128fcbb914dbd1fbc14a17
dramelia <104097195+dramelia@users.noreply.github.com>
2022-09-02 15:17:58 +0100

    Update bedmap.sh

--- bedmap.sh
+++ bedmap.sh
@@ -8,8 +8,11 @@
 
 #$ -m be
 
+export SAMPLE=<sample_id>
+export PATH=/PATH/TO/GENCODE.V19
+
 echo going to node
 
-bedmap --echo --echo-map-id-uniq --delim '\t' "selectedInsert_${SAMPLE}_merged_sorted.bed" /nobackup/medafisa/switchillumina/gencode.v19.annotation.exon.gene_shortedV2.bed > "selectedInsert_${SAMPLE}_Annotated.bed"
+bedmap --echo --echo-map-id-uniq --delim '\t' "selectedInsert_${SAMPLE}_merged_sorted.bed" /$PATH/gencode.v19.annotation.exon.gene_shortedV2.bed > "selectedInsert_${SAMPLE}_Annotated.bed"
 
 echo finished
