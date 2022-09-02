85b850f3568c75827611fab9881f26a942b184cd
dramelia <104097195+dramelia@users.noreply.github.com>
2022-09-02 16:28:48 +0100

    Update python2.sh

--- python2.sh
+++ python2.sh
@@ -8,9 +8,11 @@
 
 #$ -m be
 
+export SAMPLE=<sample_id>
+
 echo going to node
 
-python /nobackup/medafisa/switchillumina/ValidateInserts.py $SAMPLE.bam $SAMPLE 2
+python /PATH/TO/SwithScriptsIllumina/ValidateInserts.py $SAMPLE.bam $SAMPLE 2
 
 echo finished
 
