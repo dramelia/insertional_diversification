2b42f408843827b9f74984315c438b722828ddb0
dramelia <104097195+dramelia@users.noreply.github.com>
2022-09-02 15:34:28 +0100

    Update bwa.sh

--- bwa.sh
+++ bwa.sh
@@ -8,6 +8,8 @@
 
 #$ -m be
 
+#If using bwa for the first time in the current environment ensure bwa is indexed.  
+
 echo going to node
 
 bwa mem -t 10 /nobackup/medafisa/hg19/hg19.fa /nobackup/medafisa/data/$SAMPLE/CleaningWithTrimGalore/$VAL1 /nobackup/medafisa/data/$SAMPLE/CleaningWithTrimGalore/$VAL2 | samtools view -bSu - > $SAMPLE.notSorted.bam
